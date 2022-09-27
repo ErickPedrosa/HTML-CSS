133df39e3b75c81f014fc94ab3f89521e52165e6
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-09-27 08:59:07 -0300

    update

--- Alura/Flex-e-Grid/index.js
+++ Alura/Flex-e-Grid/index.js
@@ -2,7 +2,6 @@ const botaoMenu = document.querySelector(".cabecalho__menu");
 const menu = document.querySelector(".menu-lateral");
 
 botaoMenu.addEventListener('click',()=>{
-    menu.classList.toggle('menu-lateral--ativo')
+    menu.classList.toggle('menu-lateral--ativo');
 })
-
 
