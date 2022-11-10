438baf411583496e269d2f35d7c1a86f5f0ade42
Erick Pedrosa <84411590+ErickPedrosa@users.noreply.github.com>
2022-11-10 19:14:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+var noturno = false;
+
+
+function trocaSite(){
+    if (noturno == false) {
+        noturno = true;
+
+
+        icone.src = "../../../HTML-CSS/icons/lua.png";
+
+    }else{
+        noturno = false;
+
+        icone.src = "../../../HTML-CSS/icons/sol.png";
+
+    }
+
+    body.classList.toggle("noturno");
+
+    for(let counter = 0; counter < linkBotao.length; counter++){
+        linkBotao[counter].classList.toggle("noturno");
+    }
+    
+}
+
+
+
+const botao = document.querySelector("#botao");
+const body = document.querySelector("body");
+const linkBotao = document.querySelectorAll(".link-botao");
+const icone = document.querySelector(".icone");
+
+
+botao.onclick = function () { trocaSite() }
